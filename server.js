98b54ad5dae1b64f32a952a7a80a9e6156fbf778
Renato Viana <renatoviana30@gmail.com>
2020-05-27 21:58:01 -0300

    Isolando o Express e Nodemon

--- server.js
+++ server.js
@@ -1,33 +1,8 @@
-const http = require('http');
+const app = require('./src/config/custom-express');
 
-const server = http.createServer((req, resp) => {
-
-    let html = '';
-    if (req.url == '/') {
-        html = `
-        <html>
-                <head>
-                    <meta charset="utf-8">
-                </head>
-                <body>
-                    <h1> Casa do Código </h1>
-                </body> 
-            </html>
-        `;
-    } else if(req.url == '/livros') {
-        html = `
-        <html>
-                <head>
-                    <meta charset="utf-8">
-                </head>
-                <body>
-                    <h1> Listagem de Livros </h1>
-                </body> 
-            </html>
-        `;
-    }
-
-    resp.end(html);
+app.listen(3000, () => {
+    console.log(`Servidor rodando na porta 3000`);
 });
 
-server.listen(3000);
+
+
---
+++ src/app/rotas/rotas.js
@@ -0,0 +1,32 @@
+module.exports = (app) => {
+
+    app.get('/', (req, resp) => {
+        resp.send(
+            `
+            <html>
+                    <head>
+                        <meta charset="utf-8">
+                    </head>
+                    <body>
+                        <h1> Casa do Código </h1>
+                    </body> 
+                </html>
+            `
+        );
+    });
+
+    app.get('/livros', (req, resp) => {
+        resp.send(
+            `
+            <html>
+                    <head>
+                        <meta charset="utf-8">
+                    </head>
+                    <body>
+                        <h1> Listagem de Livros </h1>
+                    </body> 
+                </html>
+            `
+        );
+    });
+};
